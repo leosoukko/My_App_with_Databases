78e6d51426ac093b4d7ed48e58abf0f4bf3b507a
Leo Soukko <leo.soukko@gmail.com>
2020-09-13 22:05:27 +0300

    gitignore added

--- london_stock_data.py
+++ london_stock_data.py
@@ -11,33 +11,24 @@ class london_stock_exchange:
     def __init__(self,stock_file):
         self.stock_file=stock_file
 
-    def get_trade_data(self,stockName=False,interval=1):
-        # if stock tick is not given in visualize.py
-        if stockName==False:
-            # biggest key (most frequent stock)
-            stockName=self.stockName
-        # get data from API
-        site=requests.get('https://api.londonstockexchange.com/api/gw/lse/download/{}/trades'.format(stockName))
-        if site.ok:
-            # data as df, limit the data for this date (overnight trades not available)
-            self.df=pd.read_csv(StringIO(site.text),index_col=0)
-            self.df.index=pd.to_datetime(self.df.index)
-            now=pd.to_datetime(datetime.date.today())
-            self.df=self.df[self.df.index>=now]
-            # change correct GBPX to GBP and get only trades in pounds
-            self.df.loc[(self.df['currency']=='GBX'),'price']=self.df.loc[(self.df['currency']=='GBX'),'price']/100.0
-            self.df=self.df[(self.df['currency']=='GBP') | (self.df['currency']=='GBX')]
-            # need to drop the first trade of the day because it has much higher volume than other trades
-            self.df=self.df.drop(self.df['volume'].idxmax(),axis=0)
+    def read_stock_ticks(self):
+        self.stocks=pd.read_csv(self.stock_file,header=None).iloc[:,0].values
 
-            # news articles of this stock, limit for this date
-            self.lon_news_df=pd.DataFrame(self.lon_news[stockName])
-            self.lon_news_df.index=self.lon_news_df['date']
-            self.lon_news_df=self.lon_news_df.drop(columns=['date'])
-            self.lon_news_df.index=pd.to_datetime(self.lon_news_df.index)
+    def get_trade_data(self):
+        for stock in self.stocks:
+            # get data from API
+            site=requests.get('https://api.londonstockexchange.com/api/gw/lse/download/{}/trades'.format(stock))
+            if site.ok:
+                # data as df, limit the data for this date (overnight trades not available)
+                self.df=pd.read_csv(StringIO(site.text),index_col=0)
+                self.df.index=pd.to_datetime(self.df.index)
+                now=pd.to_datetime(datetime.date.today())
+                self.df=self.df[self.df.index>=now]
+                # change GBX to GBP and get only trades in pounds
+                self.df.loc[(self.df['currency']=='GBX'),'price']=self.df.loc[(self.df['currency']=='GBX'),'price']/100.0
+                self.df['currency'][self.df['currency']=='GBX']='GBP'
+                #self.df=self.df[(self.df['currency']=='GBP') | (self.df['currency']=='GBX')]
 
-            print('Fetched',stockName,': df size',self.df.shape)
-        else:
-            del self.lon_tickFreqs[stockName]
-            self.lon_tickNames.remove(stockName)
-            print('Failed to fetch trade data')
+                print('Fetched',stock,': df size',self.df.shape)
+            else:
+                print('Failed to fetch trade data')
