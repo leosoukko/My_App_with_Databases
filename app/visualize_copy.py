79880ca10e72a6087e0e9087f36d01947d284dd1
Leo Soukko <leo.soukko@gmail.com>
2020-09-15 15:12:49 +0300

    viz

--- app/visualize_copy.py
+++ app/visualize_copy.py
@@ -15,26 +15,20 @@ import plotly.graph_objects as go
 import pandas as pd
 # in python
 import random
+import datetime
 
 ############################################## INITIALIZATION #############################################
-class create_app:
-    def __init__(self,config_file):
-        self.config_file=config_file
+config_file='../config/database_config.json'
 
-    def connect_to_db(self):
-        # connect to postgre
-        self.psql=postgre_connection.connect_to_postgre(self.config_file)
-        self.psql.connect_to_db()
-        # get columns (assumed that they are the same for all tables, which is currently true)
-        query="""SELECT column_name FROM information_schema.columns WHERE table_schema='public' AND table_name='{}';""".format(psql.tables[0])
-        res=psql.engine.execute(query).fetchall()
-        self.psql_columns=[col[0] for col in res]
-        # connect to mongo
-        self.mongo=mongodb_connection.connect_to_mongodb(self.config_file,database_name='news')
-        self.mongo.connect_to_db()
-
-################################################ LAYOUT #############################################
-def create_layout()
+psql=postgre_connection.connect_to_postgre(config_file)
+psql.connect_to_db()
+# get columns (assumed that they are the same for all tables, which is currently true)
+query="""SELECT column_name FROM information_schema.columns WHERE table_schema='public' AND table_name='{}';""".format(psql.tables[0])
+res=psql.engine.execute(query).fetchall()
+psql_columns=[col[0] for col in res]
+# connect to mongo
+mongo=mongodb_connection.connect_to_mongodb(config_file)
+mongo.connect_to_db()
 
 ################################################# APP #############################################
 app = dash.Dash(__name__,title='MyApp')
@@ -44,13 +38,10 @@ app.layout = html.Div(children=[
     html.H1(children='MyApp'),
 
     html.Div(children='''
-    Most frequent words used in Reuters, MarketWatch and Financial Times sitemap.xml file article titles are visualized in the first figure.
-    One can examine either titles or tickers aggregated on a given time interval. N most common results can be shown.
+    
     '''),
     html.Div(children='''
-    In the second figure trades of a stock in London Stock Exchange (that was mentioned in the sitemaps) are shown with some articles
-    related to it. One can choose between different stocks and choose which attribute is on the y-axis or as the color. Hovering
-    over star shaped markers reveal the article titles.
+    
     '''),
     ],
     className='allText'
@@ -66,18 +57,43 @@ app.layout = html.Div(children=[
     clearable=False
     ),
     html.Div(id='london-asset-dropdown-output'),
-    # Filter data
-    html.Div('Filter data based on columns',className='allText'),
-    london_filters=[]
-    for col in psql_columns:
-            html.Div(dcc.Dropdown(
-            id='london-asset-filter-dropdown'.format(col),
-            options=[{'label': i, 'value': i} for i in psql_columns],
-            value=psql_columns[0],
-            className='dropdown',
-            clearable=True
-            ),
-            html.Div(id='london-asset-filter-dropdown-output'.format(col)))
+    # date range start
+    html.Div("""
+    Filter data with starting and ending dates (format YYYY-MM-DD HH:MM:SS)<br>
+    And with minimum and maximum volumes
+    """,className='allText'),
+    dcc.Input(
+        id='london-start-date',
+        type='text',
+        value='2020-09-01 00:00:00',
+        className='writable_input',
+        debounce=True
+    ),
+    # date range end
+    dcc.Input(
+        id='london-end-date',
+        type='text',
+        value=datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f"),
+        className='writable_input',
+        debounce=True
+    ),
+    # filter based on volume
+    dcc.Input(
+        id='london-volume-filter-min',
+        type='number',
+        value=1,
+        min=1,
+        className='writable_input',
+        debounce=True
+    ),
+    dcc.Input(
+        id='london-volume-filter-max',
+        type='number',
+        value=100000,
+        min=2,
+        className='writable_input',
+        debounce=True
+    ),
     # placeholder for graph
     dcc.Graph(
         id='london-graph'
@@ -85,7 +101,34 @@ app.layout = html.Div(children=[
 ])
 ############################################## CALLBACKS #############################################
 ############################################## TRADE FIGURE #############################################
-@app.callback(Output('word-graph','figure'),
-[Input('word-freq-dropdown','value'),
-Input('word-freq-time-interval','value'),
-Input('word-freq-top-n','value'),])
+@app.callback(Output('london-graph','figure'),
+[Input('london-asset-dropdown','value'),
+Input('london-start-date','value'),
+Input('london-end-date','value'),
+Input('london-volume-filter-min','value'),
+Input('london-volume-filter-max','value')])
+
+# update the figure
+def update_london_fig(assetName,startDate,endDate,volumeMin,volumeMax):
+    # make a query to postgre db
+    query="""
+    SELECT * FROM "{}" WHERE "tradeTime">='{}' AND "tradeTime"<='{}' AND "volume">={} AND "volume"<={} ORDER BY "tradeTime" DESC;
+    """.format(assetName,startDate,endDate,volumeMin,volumeMax)
+    df=pd.read_sql(query,con=psql.engine,index_col=['tradeTime'])
+    df.index=pd.to_datetime(df.index)
+
+    # make the figure
+    fig = go.Figure(go.Scattergl(x=df.index,y=df['price'],marker_color=df['volume'],mode='markers',
+    marker=dict(colorscale='Bluered',showscale=True),text=df['volume'],
+    hovertemplate='price: %{y}'+'<br>time : %{x}<br>volume: %{text}'))
+    # layout
+    fig.update_layout(title=dict(text=assetName,x=0.5,y=0.87))
+    #fig.update_layout(legend=dict(yanchor="top",y=0.99,xanchor="left",x=0.01))
+    fig.update_yaxes(title_text='price')
+    fig.update_xaxes(nticks=31,tickangle=300)
+
+    return fig
+
+############################################################### DEBUG #############################################
+if __name__ == '__main__':
+    app.run_server(debug=True)
